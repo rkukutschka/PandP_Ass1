174ff399a6d1eeda513522768d4b4c34c6970e8e
p-unger <Unger@Philips-MacBook-Pro.local>
2016-02-22 10:46:35 +0100

    Main-file

---
+++ Assignment1_rscript.R
@@ -0,0 +1,8 @@
+# Assignment 1 
+
+library(ggplot2)
+library(repmis)
+
+possiblewd <- c("~/Dropbox/4_Spring_2016/2_Collaborative Data Analysis/", 
+                "~/Documents/Collaborative Social Science/Collaborative Analysis Assignments/PandP_Ass1")
+set_valid_wd(possiblewd)
