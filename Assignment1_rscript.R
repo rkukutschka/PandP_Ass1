a49cd64b8bb6e93d234eef8462da9702700d827e
p-unger <Unger@Philips-MacBook-Pro.local>
2016-02-23 22:33:36 +0100

    Attempting to make it work :/

--- Assignment1_rscript.R
+++ Assignment1_rscript.R
@@ -5,6 +5,7 @@
 # Loading packages
 library(ggplot2)
 library(repmis)
+library(plyr)
 
 # Set working directory
 possiblewd <- c("~/Dropbox/4_Spring_2016/2_Collaborative Data Analysis/", 
@@ -14,6 +15,18 @@ set_valid_wd(possiblewd)
 # Load dataset
 occStatusDataFrame <- as.data.frame(occupationalStatus)
 
+# View the data
+View(occStatusDataFrame)
+
+# Summarize frequencies over origin
+tapply(occStatusDataFrame$Freq, occStatusDataFrame$origin, FUN=sum)
+
+# Summarize frequencies over destination
+tapply(occStatusDataFrame$Freq, occStatusDataFrame$destination, FUN=sum)
+
+# Create vectors
+origin <- tapply(occStatusDataFrame$Freq, occStatusDataFrame$origin, FUN=sum)
+hist(origin)
 
 # Source external analysis on Titanic dataset 
 source("PandP_Ass1/Analysis_1.R")
