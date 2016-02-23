4ef8606ea141b46a5c9726a75f4cc21e5f2cf65b
p-unger <Unger@Philips-MacBook-Pro.local>
2016-02-23 16:17:29 +0100

    Update #1

--- Assignment1_rscript.R
+++ Assignment1_rscript.R
@@ -1,14 +1,18 @@
+#####
 # Assignment 1 
+#####
 
+# Loading packages
 library(ggplot2)
 library(repmis)
 
-
+# Creating relative file-paths
 possiblewd <- c("~/Dropbox/4_Spring_2016/2_Collaborative Data Analysis/", 
                 "~/Documents/Collaborative Social Science/Collaborative Analysis Assignments/PandP_Ass1")
 
 set_valid_wd(possiblewd)
 
+# Source external analysis on Titanic dataset 
 source("PandP_Ass1/Analysis_1.R")
 
-
+data()
